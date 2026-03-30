08e7bbd6ec5a8ca26ec9aa93ebec8c66ea70a7c3
Harish-HK1895 <harishkumar.s@yjktechnologies.com>
2026-03-30 18:25:20 +0530

    30-03-2026 evening changes

--- src/Main.js
+++ src/Main.js
@@ -126,6 +126,7 @@ import EmpDocumentReq from "./ESSComponents/EmpDocumentReq.js"
 import EmpAssetsRequest from "./ESSComponents/EmpAssetsRequest.js"
 import LoanSummaryReports from "./LoanSummaryReports.js"
 import PendingApprovalsReport from "./PendingApprovalsReport.js"
+import LoanDisbursementRepo from "./LoanDisbursementReport.js"
 
 import { ToastContainer } from "react-toastify";
 import GenerateShift from "./ShiftMaster/GenerateShift.js";
@@ -328,6 +329,7 @@ function Main() {
     { path: "/EmpAssetsRequest", component: <EmpAssetsRequest /> },
     { path: "/LoanSummaryReports", component: <LoanSummaryReports /> },
     { path: "/PendingApprovalsRepo", component: <PendingApprovalsReport /> },
+    { path: "/LoanDisbursementRepo", component: <LoanDisbursementRepo /> },
 
   
   ];
