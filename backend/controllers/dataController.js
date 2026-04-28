ec100281b1d78f840e30b8744669fecdaf7ce984
Dinesh-Gokul <dinesh.gokul@avvaitech.com>
2026-04-28 18:02:56 +0530

    28-04-2026 evening commit

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -48475,7 +48475,7 @@ const shiftChangeRequestManager = async (req, res) => {
 };
 
 const shiftRequestManagerApproval = async (req, res) => {
-  const { request_id, company_code, request_status } = req.body;
+  const { request_id, company_code, request_status, modified_by } = req.body;
 
   try {
     const pool = await sql.connect(dbConfig);
@@ -48486,7 +48486,8 @@ const shiftRequestManagerApproval = async (req, res) => {
       .input("request_id", sql.Int, request_id)
       .input("company_code", sql.NVarChar, company_code)
       .input("request_status", sql.NVarChar, request_status)
-      .query(`EXEC sp_shift_change_requests @mode,@request_id,'','','','','','','','','','',@request_status,'','','','','',@company_code,'','','',''`);
+      .input("modified_by", sql.NVarChar, modified_by)
+      .query(`EXEC sp_shift_change_requests @mode,@request_id,'','','','','','','','','','',@request_status,'','','','','',@company_code,'','',@modified_by,''`);
 
     res.status(200).json({
       success: true,
