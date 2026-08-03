fe1793d7bab16b0e88f11efbd56f785243f13c63
Dinesh-Gokul <dinesh.gokul@avvaitech.com>
2026-08-03 17:37:26 +0530

    3-08-2026 Evening Changes

--- src/EmpRequests/ShiftChangeRequest.js
+++ src/EmpRequests/ShiftChangeRequest.js
@@ -347,8 +347,8 @@ const ShiftChangeRequest = () => {
                     employee_id: sessionStorage.getItem('selectedUserCode'),
                     shift_from_date: effectiveFromDate ? effectiveFromDate : null,
                     shift_to_date: effectiveToDate ? effectiveToDate : null,
-                    current_shift_id: setCurShiftSc,
-                    requested_shift_id: setReqShiftSc,
+                    current_shift_id: curShiftSc,
+                    requested_shift_id: reqShiftSc,
                     is_swap_request: empStatusSc,
                     request_status: manStatusSc,
                 })
@@ -446,7 +446,7 @@ const ShiftChangeRequest = () => {
             toast.error("Error fetching search data: " + error.message);
         }
     };
-
+                        
     const empShiftCols = [
         {
           headerName: "S.No",
